3bd26b7a40ef1fe461efd50f2d4fb62df7998bf5
chandanB24 <106895903+chandanB24@users.noreply.github.com>
2024-03-12 15:37:38 +0530

    pushing operators

---
+++ Operators/index.js
@@ -0,0 +1,29 @@
+//Operators in js
+
+//addition operators
+var a = 1;
+var b = 2;
+console.log(a + b) //output : 3
+
+// '+' operator is also used to concat string-string, string-number, number-string
+console.log(1 + "1"); //output : 11
+console.log("chandanb" + 24); //output : chandanb24
+console.log("iVoyant" + "Systems"); //output : iVoyantSystems
+console.log(1 + + "1") //output : 2 because of implicit conversion
+
+//Mathematical operator
+console.log(3 - 5);     // outputs : -2
+console.log(3 * 5);     // outputs : 15
+console.log(3 / 5);     // outputs : 0.6
+
+//modulo operator
+console.log(5 % 3);     // outputs 2
+//abs() return the absoulte value of the number if
+console.log(Math.abs(-7.99)) //output 7.99
+console.log(Math.abs("hello")) //output : NaN
+console.log(Math.exp(-6)) //output : 0.0024787521766663585
+console.log(Math.pow(2,3)) //output : 8
+console.log(Math.floor(7.999)) //output : 7
+console.log(Math.random(10)) //output : 0.372361304762294  random number x 0<x<1
+
+
