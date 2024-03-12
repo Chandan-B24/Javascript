c14b9685e1df21a541dc70ad03aa86952f6c2dac
chandanB24 <106895903+chandanB24@users.noreply.github.com>
2024-03-12 17:35:15 +0530

    Pushing the popbox

---
+++ Popbox/index.js
@@ -0,0 +1,7 @@
+// Popbox or alert like boxes in JS
+//confirm boxes will return true if it's ok return false on canceled
+confirm("Hi")
+//prompt box will return whatever the text written in the prompt
+prompt("enter you name")
+//alert it will not return anything just a message
+alert("are you sure want to delete?")
