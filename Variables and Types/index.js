4ef419bbabf6b5d3eae00061e504ef69911e0c5b
chandanB24 <106895903+chandanB24@users.noreply.github.com>
2024-03-12 14:03:44 +0530

    Variables and Types

---
+++ Variables and Types/index.js
@@ -0,0 +1,30 @@
+// variables in javascript
+
+var name = "Chandan B";
+var age  = 21;
+var email = "chandanb24002@gmail.com"
+
+//types in javascript
+
+//number type it may contain integers,double,floating numbers
+const myNumber = 9986723172;
+//string type
+const companyName = "iVoyant Systems";
+//boolean type
+const isWorking = true;
+//array type
+const fruits = ['Apple','Banana','Watermelon','Pineapple','Grapes'];
+//object type
+const car ={
+    name : "Tata nexon",
+    modelYear : 2023,
+    isElectric : false,
+    mileage : '15Km/l',
+    onRoadPrice : 1237500
+}
+//undefined 
+var myVariable;
+console.log(myVariable) //output : undefined
+//null only used when the variable value should be shown as empty
+const myVaribaleNull = null;
+console.log(myVaribaleNull) //output : null
